c475e8ffa63f0e22641ff9e2815bd0adb59f3b5a
孙永飞 <sunyongfei@xiaohongshu.com>
2024-09-19 20:09:38 +0800

    perf: xxx

--- build_run.sh
+++ build_run.sh
@@ -2,4 +2,4 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 g++ -shared -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux profiler.cpp -o libprofiler.so -ldl
 javac Profiler.java
-java -agentpath:/path/to/libprofiler.so Profiler
+java -agentpath:./libprofiler.so Profiler
